f0a40b32e07bd1f6b5eee3ff59ffdb13b091d04a
Philippe Leefsma <philippe.leefsma@gmail.com>
2018-06-27 11:41:21 +0200

    update

--- src/client/components/Viewer.Components/Extensions/Dynamic/Viewing.Extension.OpenCV/Viewing.Extension.OpenCV.js
+++ src/client/components/Viewer.Components/Extensions/Dynamic/Viewing.Extension.OpenCV/Viewing.Extension.OpenCV.js
@@ -66,7 +66,7 @@ export default class OpenCVExtension
   //
   //
   /////////////////////////////////////////////////////////
-  onModelRootLoaded () {
+  onObjectTreeCreated () {
 
     const nav = this.viewer.navigation
     
